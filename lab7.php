520f42db536bca90dc61a4d37430d8e9a12c9459
Joel Roytson Menezes <83909485+joelrm11@users.noreply.github.com>
2022-07-19 07:25:11 +0530

    Add files via upload

---
+++ lab7.php
@@ -0,0 +1,18 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+ <meta http-equiv="refresh" content="1"/>
+ <style>
+ p{
+		color:red;
+	
+		padding-left:400px;
+		font-size:100px;
+ }
+ </style>
+</head>
+<body>
+
+ <p> <?php echo date(" h: i : s A");?> </p>
+</body>
+<html>
